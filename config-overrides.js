d3ea37e9743cd526983ac805798822cc2eb400ac
Artem Mehes <artemmeges@gmail.com>
2021-01-16 17:38:52 +0200

    updates

--- config-overrides.js
+++ config-overrides.js
@@ -6,7 +6,6 @@ module.exports = function override(config, env) {
   // eslint-disable-next-line no-param-reassign
   config = rewireAliases.aliasesOptions({
     '@assets': path.resolve(__dirname, `${paths.appSrc}/app/assets`),
-    '@styles': path.resolve(__dirname, `${paths.appSrc}/app/styles`),
     '@components': path.resolve(__dirname, `${paths.appSrc}/app/components`),
     '@pages': path.resolve(__dirname, `${paths.appSrc}/app/pages`),
     '@utils': path.resolve(__dirname, `${paths.appSrc}/app/utils`),
--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import '@assets/fonts/index.css';
-import GlobalStyles from '@styles/GlobalStyles';
 import Home from '@pages/Home';
 import { ThemeProvider } from 'styled-components';
+import GlobalStyles from './app/styles/GlobalStyles';
 import theme from './theme';
 
 ReactDOM.render(
